58601faa3ca2dce6cf220539ccd6efd23c3025d8
Joshua dela Cruz <jgdelacruz5@gmail.com>
2023-05-02 13:05:09 +0800

    Add test project for lambda

---
+++ src/lambda.ts
@@ -0,0 +1,22 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { S3 } from "aws-sdk";
+
+const s3 = new S3();
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  const bucket = process.env.BUCKET_NAME || "";
+
+  try {
+    const list = await s3.listObjects({ Bucket: bucket }).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(list),
+    };
+  } catch (error) {
+    const message = (error as Error).message;
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message }),
+    };
+  }
+};
