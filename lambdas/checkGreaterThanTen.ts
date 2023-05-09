b222cd57436243c4083a3ad00d7f7f67d7035ff2
Joshua dela Cruz <jgdelacruz5@gmail.com>
2023-05-09 08:39:09 +0800

    log context

--- lambdas/checkGreaterThanTen.ts
+++ lambdas/checkGreaterThanTen.ts
@@ -1,11 +1,12 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 
-export const handler: APIGatewayProxyHandler = async (event: any) => {
+export const handler: APIGatewayProxyHandler = async (event: any, context: any) => {
+  console.log(context, event);
   const number = parseInt(event.queryStringParameters.result, 10);
   const isGreaterThanTen = number > 10;
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ isGreaterThanTen }),
+    body: JSON.stringify({ result: 1 }),
   };
 };
--- lambdas/multiplyByTwo.ts
+++ lambdas/multiplyByTwo.ts
@@ -2,11 +2,11 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 
 export const handler: APIGatewayProxyHandler = async (event: any, context: any) => {
   console.log(context, event);
-  const number = parseInt(event.queryStringParameters.inputNumber, 10);
-  const result = number * 2;
+  // const number = parseInt(event.queryStringParameters.inputNumber, 10);
+  // const result = number * 2;
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ result }),
+    body: JSON.stringify({ result: 2 }),
   };
 };
