9192ca03f438699760bbd4fbedca3b70a39f4527
Joshua dela Cruz <jgdelacruz5@gmail.com>
2023-05-09 08:26:48 +0800

    Add logger

--- lambdas/multiplyByTwo.ts
+++ lambdas/multiplyByTwo.ts
@@ -1,6 +1,7 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 
-export const handler: APIGatewayProxyHandler = async (event: any) => {
+export const handler: APIGatewayProxyHandler = async (event: any, context: any) => {
+  console.log(context, event);
   const number = parseInt(event.queryStringParameters.inputNumber, 10);
   const result = number * 2;
 
