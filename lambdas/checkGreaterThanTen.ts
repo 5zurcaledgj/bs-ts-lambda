43b4ca8aa0737d044ca760101249549a36a5d4c9
Joshua dela Cruz <jgdelacruz5@gmail.com>
2023-05-09 09:33:34 +0800

    Change the result

--- lambdas/checkGreaterThanTen.ts
+++ lambdas/checkGreaterThanTen.ts
@@ -1,6 +1,4 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-
-export const handler: APIGatewayProxyHandler = async (event: any, context: any) => {
+export const handler = async (event: any, context: any) => {
   console.log(context, event);
   const number = parseInt(event.body.result, 10);
   const isGreaterThanTen = number > 10;
